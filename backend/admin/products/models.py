90dbf1485c73ce0ddfd8590111bbc6dc22ef3aeb
maheh <maheshmb7712@gmail.com>
2022-07-30 10:56:17 +0530

    connected with mysql and containarized

---
+++ backend/admin/products/models.py
@@ -0,0 +1,13 @@
+from operator import mod
+from statistics import mode
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title=models.CharField(max_length=200)
+    image=models.CharField(max_length=200)
+    likes=models.PositiveIntegerField(default=0)
+
+class User(models.Model):
+    pass
