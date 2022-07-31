10d209eab0bc883ba34b5ef6be3f04e161e7d50c
maheh <maheshmb7712@gmail.com>
2022-07-31 20:50:27 +0530

    from scratch

--- backend/admin/products/models.py
+++ backend/admin/products/models.py
@@ -1,14 +1,11 @@
-from operator import mod
-from statistics import mode
-from turtle import title
 from django.db import models
 
 
-
 class Product(models.Model):
-    title=models.CharField(max_length=200)
-    image=models.CharField(max_length=200)
-    likes=models.PositiveIntegerField(default=0)
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    likes = models.PositiveIntegerField(default=0)
+
 
 class User(models.Model):
     pass
---
+++ backend/main/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqp://guest:guest@host.docker.internal:5672?connection_attempts=10&retry_delay=10')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
